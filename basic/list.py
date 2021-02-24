7b128e26b3075074d93b89370094df5252223254
sugeryp <aki.sugeryp@gmail.com>
2021-02-24 19:38:35 +0900

    g

--- basic/list.py
+++ basic/list.py
@@ -1,2 +1,136 @@
 l = [1, 20, 4, 50, 2, 1, 2]
 l = [1, 20, 4, 50, 2, 1, 2]
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+a = ['a', 'b', 'c']
+b = [1, 2, 3]
+c = [a, b]
+
+print(f"""\
+{l[1]}
+{l[-1]}
+{l[-2]}
+{l[0:2]}
+{l[:2]}
+{l[2:5]}
+{l[2:]}
+{l[:]}
+{len(l)}
+{list('abcdefg')}
+
+{n}
+{n[::2]}
+{n[::-1]}
+{n[::-2]}
+
+{c}
+{c[0]}
+{c[0][1]}
+\
+""")
+print('###################################################')
+
+print('###################################################')
+s = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+print(s[0])
+s[0] = 'X'
+print(s[0])
+s[2:5] = ['C', 'D', 'E']
+print(s)
+s[2:5] = []
+print(s)
+s[:] = []
+print(s)
+print(n)
+n.append(100)
+print(n)
+n.insert(0, 200)
+print(n)
+n.pop(0)
+print(n)
+del n[0]
+print(n)
+if 'n' in globals():
+    print('exist s beyond')
+del n
+if 'n' in globals():
+    print('exist s after')
+
+n = [1, 2, 2, 2, 3]
+n.remove(2)
+print(n)
+n.remove(2)
+n.remove(2)
+print(n)
+
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+
+a += b
+print(a)
+
+del a[5:]
+print(a)
+
+a.extend(b)
+print(a)
+
+print('###################################################')
+
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3))
+print(r.index(3, 3))
+
+print(r.count(3))
+
+if 5 in r:
+    print('exist')
+
+print(r)
+r.sort()
+print(r)
+
+r.reverse()
+print(r)
+
+r.sort(reverse=True)
+print(r)
+
+r.reverse()
+print(r)
+
+s = 'My name is Mike'
+to_split = s.split(' ')
+print(to_split)
+
+to_split2 = s.split('lerfketke')
+print(to_split2)
+
+to_join = ' '.join(to_split)
+print(to_join)
+to_join = '####'.join(to_split)
+print(to_join)
+# print(help(list))
+
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j =', j)
+print('i =', i)
+print(id(i))
+print(id(j))
+
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+x[0] = 100
+print('x=', x)
+print('y=', y)
+print(id(x))
+print(id(y))
+
+x = [1, 2, 3, 4, 5, 6]
+y = x[:]
+x[0] = 100
+print('x=', x)
+print('y=', y)
---
+++ basic/string.py
@@ -0,0 +1,95 @@
+print('###################################################')
+print("""
+line1
+line2
+line3
+""")
+print('###################################################')
+print('###################################################')
+print("""\
+line1
+line2
+line3\
+""")
+print('###################################################')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
+
+s2 = ('aaaaaaaaaaaaaaaaaaaaaaaaaaa\n'
+      "bbbbbbbbbbbbb'bbbb\"bbbbbbbbbb")
+
+path = ('C:\name\name')
+path2 = (r'C:\name\name')
+
+print(path)
+print(path2)
+
+
+
+
+
+
+print('\nslice##############################################')
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print(word[0:2])
+print(word[:2])
+print(word[2:])
+word = 'j' + word[1:]
+print(word)
+print(word[:])
+n = len(word)
+print(n)
+print('###################################################\n')
+
+
+
+
+
+print('\nmethod##############################################')
+s = 'My name is Mike. Hi Mike'
+print(s)
+is_start = s.startswith('My')
+print(is_start)
+is_start = s.startswith('Mike')
+print(is_start)
+
+# 前のMike
+print(s.find('Mike'))
+# 後ろのMike
+print(s.rfind('Mike'))
+
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
+
+print('#####################################################\n')
+
+
+print('format#################################################')
+
+print('My name is {} {}'.format('Akihiro', 'Kondo'))
+print('My name is {0} {1}'.format('Akihiro', 'Kondo'))
+print('My name is {1} {0}'.format('Akihiro', 'Kondo'))
+print('My name is {name} {family}'.format(name='Akihiro', family='Kondo'))
+print('{} {} {}'.format(1, 2, 3))
+print(str(1))
+
+
+l = [1, 20, 4, 50, 2, 1, 2]
+l = [1, 20, 4, 50, 2, 1, 2]
+
+print(f"""\
+{l[1]}
+{l[-1]}\
+""")
+print('###################################################')
